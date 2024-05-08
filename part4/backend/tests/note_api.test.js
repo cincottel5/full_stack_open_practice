2acff380ef556db5765ecb8cc7790f8baab4baa1
Yelko Carvajal Mora <ycarvajalm@baccredomatic.com>
2024-05-08 17:13:29 -0600

    part 4

---
+++ part4/backend/tests/note_api.test.js
@@ -0,0 +1,50 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Note = require('../models/note')
+
+const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  await new Note(initialNotes[0]).save()
+  await new Note(initialNotes[1]).save()
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+//test.only('there are two notes', async () => {
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+  const contents = response.body.map(e => e.content)
+
+  assert(contents.includes('HTML is easy'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})

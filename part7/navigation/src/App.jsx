518433885c43cfc2967ff29c17c68cc6a7dafb70
Yelko Carvajal <cincottel@gmail.com>
2025-03-25 14:29:40 -0600

    7.a

---
+++ part7/navigation/src/App.jsx
@@ -0,0 +1,52 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+  </div>
+)
+
+const Notes = () => (
+  <div>
+    <h2>Notes</h2>
+  </div>
+)
+
+const Users = () => (
+  <div>
+    <h2>Users</h2>
+  </div>
+)
+
+const App = () => {
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes/>} />
+        <Route path="/users" element={<Users/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2024</i>
+      </div>
+    </Router>
+  )
+}
+
+export default App

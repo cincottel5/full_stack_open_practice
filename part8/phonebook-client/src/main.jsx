10ade6a07681f1a1e5470e909c8b945cb1d75a7b
Yelko Carvajal <cincottel@gmail.com>
2025-05-25 23:01:25 -0600

    6.d

---
+++ part8/phonebook-client/src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+// const query = gql`
+//   query {
+//     allPersons { 
+//       name
+//       phone
+//       address {
+//         street
+//         city
+//       }
+//       id
+//     }
+//   }
+// `
+
+// client.query({ query })
+//   .then( response => {
+//     console.log(response.data)
+//   })
+
+createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
